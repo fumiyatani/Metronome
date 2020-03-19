5a3c8ddd95a22290f66598efe55570676fbb59c8
tanifumiya <fumiyadeveloper@gmail.com>
2020-03-20 00:01:06 +0900

    プロジェクトをAndroidXに対応

--- app/src/main/java/jp/androidbook/meteonorm/BaseContentActivity.kt
+++ app/src/main/java/jp/androidbook/meteonorm/BaseContentActivity.kt
@@ -1,6 +1,6 @@
 package jp.androidbook.meteonorm
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 
 open class BaseContentActivity : AppCompatActivity() {
 
--- app/src/main/java/jp/androidbook/meteonorm/FirstContactActivity.kt
+++ app/src/main/java/jp/androidbook/meteonorm/FirstContactActivity.kt
@@ -2,7 +2,7 @@ package jp.androidbook.meteonorm
 
 import android.content.Intent
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_first_contact.*
 
